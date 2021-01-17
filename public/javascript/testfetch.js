eafc3c807277d6b2ef1de6a01639a4588e841cfd
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2021-01-16 20:32:28 -0800

    pog

--- public/javascript/testfetch.js
+++ public/javascript/testfetch.js
@@ -1,4 +1,7 @@
-fetch('https://us-central1-reading-rainbow.cloudfunctions.net/stringer?url=https://www.royalroad.com/fiction/21220/mother-of-learning/chapter/455877/afterword')
-  .then((response) => { response.json() })
-  .then((data) => { console.log('data') })
-  .catch((err) => {console.log('fetch.error:' + err)});
+function test() {
+  let url = 'https://us-central1-reading-rainbow.cloudfunctions.net/stringer?url=https://www.royalroad.com/fiction/21220/mother-of-learning/chapter/455877/afterword';
+  fetch(url)
+    .then(response => response.text())
+    .then(contents => console.log(contents))
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+}
