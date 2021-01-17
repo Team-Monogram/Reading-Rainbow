fb98c5a41d683f932aaa5ab8385a5f4349b7b120
Bchou7 <bchou7@berkeley.edu>
2021-01-16 22:20:17 -0800

    stuff

--- public/javascript/getchapter.js
+++ public/javascript/getchapter.js
@@ -1,15 +1,16 @@
 // getchapter.js will fetch the scripter cloud function by getting chapter
 // information with url parameters
 
-function get_chapter (url) {
+function get_chapter(url) {
+  console.log('Hello World');
   url = 'https://us-central1-reading-rainbow.cloudfunctions.net/stringer?url=' + url;
   fetch(url)
     .then((response) => {
       response.text();
     })
-    .then((contents)) => {
-      console.log(typeof(contents));
-    }
+    .then((contents) => {
+      document.getElementById("content").innerhtml = contents;
+    })
     .catch(() => {
       console.log('error has occurred');
     })
