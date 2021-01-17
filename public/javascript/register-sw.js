96d3dcfe0ce3385e4e37f6330322b8bbd99729f1
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2021-01-17 04:15:26 -0800

    Update register-sw.js

--- public/javascript/register-sw.js
+++ public/javascript/register-sw.js
@@ -2,7 +2,7 @@ if (navigator.serviceWorker.controller) {
   console.log("Active Service worker found");
 } else {
   navigator.serviceWorker
-    .register(serviceWorker.js, { scope: ".././" })
+    .register("./serviceWorker.js", { scope: ".././" })
     .then(function (reg) {
       console.log("Service worker registered");
     });
