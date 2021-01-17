55f9818a19e0142c811468ae000c8d75f4308e21
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2021-01-17 01:05:52 -0800

    Update serviceworker.js

--- public/javascript/serviceworker.js
+++ public/javascript/serviceworker.js
@@ -1,33 +1,8 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceworker.js')
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('Service worker registration failed, error:', error);
-    });
-  }
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
+);
 
-  self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
-var gamesImages = [];
-for(var i=0; i<games.length; i++) {
-  gamesImages.push('data/img/'+games[i].slug+'.jpg');
-}
-var contentToCache = appShellFiles.concat(gamesImages);
-
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all: app shell and content');
-        return cache.addAll(contentToCache);
-      })
-    );
-  });
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "public",
+  new workbox.strategies.NetworkFirst()
+);
